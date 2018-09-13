33af3e2ba08e8f3658c95e7a7937425e890bc9d2
Bill <skeche@gmail.com>
2018-09-13 18:06:15 -0400

    header content changes

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,3 +1,8 @@
 $(function () {
     
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function () {
+    console.log('callback - particles-js config loaded');
+});
