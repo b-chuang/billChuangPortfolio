edd50c29b6adc5b772ed4b59762e07bd7187d84d
Bill <skeche@gmail.com>
2018-09-17 23:58:59 -0400

    added styles

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -6,3 +6,27 @@ $(function () {
 particlesJS.load('particles-js', 'assets/particles.json', function () {
     console.log('callback - particles-js config loaded');
 });
+
+
+
+        //browser title
+        jQuery(document).ready(function ($) {
+            // Get page title
+            var pageTitle = $("title").text();
+
+            // Change page title on blur
+            $(window).blur(function () {
+                $("title").text("👋, 👇, come back soon");
+            });
+
+            // Change page title back on focus
+            $(window).focus(function () {
+                $("title").text(pageTitle);
+            });
+        });
+
+        //Smooth scroll 
+        $('a').smoothScroll({ offset: -1, speed: 700 });
+
+
+      
